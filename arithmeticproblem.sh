289db4bf328efcd61dbe8154c2ea6810f34a0176
SaiVenkat12 <gsaiv1001@gmail.com>
2022-11-20 23:35:54 +0530

    [SaiVenkat]

--- arithmeticproblem.sh
+++ arithmeticproblem.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 read -p "Enter the first input:" a;
 read -p "Enter the second input:" b;
 read -p "Enter the third input:" c;
@@ -6,6 +6,8 @@ read -p "Enter the third input:" c;
 z=$(( a + b * c))
 y=$(( a * b + c))
 x=$(( c + a / b))
+w=$(( a % b + c))
 echo $z
 echo $x
 echo $y
+echo $w
